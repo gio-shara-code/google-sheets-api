bcc0cf5874129a0e8eaf50e7a709060ab5810c1d
gioshara <giorgi.sharashenidze@code.berlin>
2023-02-16 15:46:51 +0100

    my actual first commit

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import { google, sheets_v4, Auth } from "googleapis";
+
+interface Credentials {
+  clientId: string;
+  clientEmail: string;
+  privateKey: string;
+  tokenUrl: string;
+}
+
+export interface GoogleSheetsApiConstructorArgs {
+  scope: "readonly" | "write";
+  projectId: string;
+  credentials: Credentials;
+  spreadSheetId: string;
+}
+
+export class GoogleSpreadSheetsApi {
+  auth: Auth.GoogleAuth;
+  spreadSheetId: string;
+  sheets: sheets_v4.Sheets;
+
+  constructor(args: GoogleSheetsApiConstructorArgs) {
+    this.sheets = google.sheets("v4");
+
+    const { credentials, projectId, scope, spreadSheetId } = args;
+    this.spreadSheetId = spreadSheetId;
+    this.auth = new google.auth.GoogleAuth({
+      projectId,
+      credentials: {
+        client_id: credentials.clientId,
+        client_email: credentials.clientEmail,
+        private_key: credentials.privateKey,
+        token_url: credentials.tokenUrl,
+      },
+      scopes:
+        scope === "readonly"
+          ? ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+          : ["https://www.googleapis.com/auth/spreadsheets"],
+    });
+  }
+
+  async updateSpreadSheet(values: any[][]) {
+    return await this.sheets.spreadsheets.values.append({
+      spreadsheetId: this.spreadSheetId,
+      auth: await this.auth.getClient(),
+      valueInputOption: "USER_ENTERED",
+      range: "Sheet1!A1:C1",
+      requestBody: {
+        range: "Sheet1!A1:C1",
+        majorDimension: "ROWS",
+        values,
+      },
+    });
+  }
+
+  async getSpreadSheetCells() {
+    return await this.sheets.spreadsheets.get({
+      spreadsheetId: this.spreadSheetId,
+      includeGridData: true,
+      auth: await this.auth.getClient(),
+    });
+  }
+}
